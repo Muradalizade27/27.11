8634960962f33244138f05795b4e688e7e749d0a
Murad Alizade <muradral@code.edu.az>
2022-11-27 21:14:05 +0400

    new HomeWork

---
+++ Task 1 27-11-22/Program.cs
@@ -0,0 +1,14 @@
+﻿using C_Methods;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "Bu bir custom methodudur";
+
+        Console.WriteLine(str.CContains("item"));
+    }
+}
+
+
+
---
+++ Task 1 27-11-22/contains.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Methods
+{
+    internal static class Contains
+    {
+        public static bool CContains(this string sentence, string search)
+        {
+
+            string toLower = sentence.ToLower();
+
+            string whiteSpace = " ";
+
+            String.IsNullOrWhiteSpace(whiteSpace);
+
+            bool startWith = toLower.StartsWith(search);
+
+            bool endWith = toLower.EndsWith(search);
+
+            string[] arrayString = toLower.Split();
+
+            foreach (var item in arrayString)
+            {
+                if (item == search)
+                {
+                    return true;
+                }
+                else if (item.ToUpper() == search.ToUpper())
+                {
+                    return true;
+                }
+                else if (startWith)
+                {
+                    return true;
+                }
+                else if (endWith)
+                {
+                    return true;
+                }
+                else if (item.StartsWith(search))
+                {
+                    return true;
+                }
+                else if (item.EndsWith(search))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
